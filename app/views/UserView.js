21b58eb2d9e0b15a1ed1abc030ea0fe9cd63fb9a
Migue Eduardo Castillo Landeros <36210519+meclajer@users.noreply.github.com>
2022-04-25 11:06:32 -0500

    UT create user by a given valid payloaad

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -1,13 +1,19 @@
+const UserService = require("../services/UserService")
+
 class UserView {
 
     static createUser(payload) {
         if (payload === null)
             return { error: "El payload no existe." }
-        if (typeof payload.userName !== 'string' || typeof payload.name !== 'string' || typeof payload.id !== 'number') {
-            console.log(typeof payload.userName + " - " + typeof payload.name + " - " + typeof payload.id)
+        else if (typeof payload.userName !== 'string' || typeof payload.name !== 'string' || typeof payload.id !== 'number') {
             return { error: "Alguna variable necesita tener un valor válido" }
             //agregar validacion para error más preciso
+        } else {
+            const user = UserService.create(payload.id, payload.userName, payload.name)
+            return user
         }
+
+
 
     }
 
