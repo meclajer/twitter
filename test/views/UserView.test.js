c79b274b5145eb34bc52d016143b4c40d4653c25
Migue Eduardo Castillo Landeros <36210519+meclajer@users.noreply.github.com>
2022-04-24 23:49:08 -0500

    UT of view created

---
+++ test/views/UserView.test.js
@@ -0,0 +1,13 @@
+describe('Test for UserView', () => {
+
+    test('Return an error object when try to create a new user with an null payload', () => {
+
+        const payload = null
+        const result = UserView.createUser(payload)
+
+        // https: //jestjs.io/docs/using-machers#strings
+        expect(result.error).toMatch("/payload no existe/")
+
+    });
+
+});
