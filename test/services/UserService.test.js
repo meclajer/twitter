e948335d05d98a6901e3fb548a5a581d7fd525d8
Migue Eduardo Castillo Landeros <36210519+meclajer@users.noreply.github.com>
2022-04-24 22:01:02 -0500

    UT getInfo() added

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -2,16 +2,30 @@ const UserService = require("../../app/services/UserService");
 
 describe('Test for UserService', () => {
 
-    test('1 create a new user using the UserService', () => {
+    test('1) Create a new user using the UserService', () => {
 
         // objt
         const myUser = UserService.create(1, "carlogilmar", "carlo")
 
-        // pru
+        // tests
         expect(myUser.userName).toBe("carlogilmar")
         expect(myUser.name).toBe("carlo")
         expect(myUser.id).toBe(1)
         expect(myUser.bio).not.toBeUndefined()
+
+    });
+
+    test('2) Get all data in a list', () => {
+
+        // objts
+        const myUserService = UserService.create(1, "carlogilmar", "carlo")
+        const myUserInfoList = UserService.getInfo(myUserService)
+
+        // tests
+        expect(myUserInfoList[0]).toBe(1)
+        expect(myUserInfoList[1]).toBe("carlogilmar")
+        expect(myUserInfoList[2]).toBe("carlo")
+        expect(myUserInfoList[3]).toBe("Hi this is my bio and this is how I'm...")
 
     });
 
